4b6ec0877a9b57524e9ebc042796d9d1fa386ab0
evgeniy.v.andrievskiy@gmail.com <evgeniy.v.andrievskiy@gmail.com@c69252fc-1634-11de-99d6-5fb3f1362b5f>
2011-03-28 03:35:39 +0000

    Old, copy in /branches

--- trunk/edu.spsu.aspectjava/src/edu/spsu/aspectjava/weaver/models/example/AspectAction.java
+++
@@ -1,9 +0,0 @@
-package edu.spsu.aspectjava.weaver.models.example;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface AspectAction {
-	String value();
-}
